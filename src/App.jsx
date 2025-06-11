87bd5db5ec918cb28890692a881143afd88920a8
Mohammad/Ebadi <mohammad.ebadi.code@gmail.com>
2025-06-11 23:30:35 +0330

    add conditional routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import About from "./pages/about/About";
 import Projects from "./pages/projects/Projects";
 import Home from "./pages/home/Home";
@@ -8,20 +8,31 @@ import ExcuseGenerator from "./components/projectsList/excuseGenerator/ExcuseGen
 import TodoList from "./components/projectsList/todoList/TodoList";
 import Calculator from "./components/projectsList/calculator/Calculator";
 import QuizApp from "./components/projectsList/quizApp/QuizApp";
+import { useState } from "react";
+import SignIn from "./pages/authentication/signIn/SignIn";
+import SignUp from "./pages/authentication/signUp/SignUp";
 
 export default function App() {
+  const [isSignedIn , setIsSignedIn] = useState(false)
+
+ const handleLogin = ()=>{
+    setIsSignedIn(true)
+  }
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home></Home>}></Route>
-        <Route path="/about" element={<About></About>}></Route>
-        <Route path="/projects" element={<Projects></Projects>}></Route>
-        <Route path="/projects/ScreenSize" element={<ScreenSize></ScreenSize>}></Route>
-        <Route path="/projects/Counter" element={<Counter></Counter>}></Route>
-        <Route path="/projects/ExcuseGenerator" element={<ExcuseGenerator></ExcuseGenerator>}></Route>
-        <Route path="/projects/TodoList" element={<TodoList></TodoList>}></Route>
-        <Route path="/projects/Calculator" element={<Calculator></Calculator>}></Route>
-        <Route path="/projects/QuizApp" element={<QuizApp></QuizApp>}></Route>
+        <Route path="/signIn" element={ isSignedIn?(<Navigate to="/"></Navigate>):(<SignIn onSignIn={handleLogin}></SignIn>)}></Route>
+        <Route path="/signUp" element={ isSignedIn?(<Navigate to="/"></Navigate>):(<SignUp></SignUp>)}></Route>
+
+        <Route path="/" element={ isSignedIn ?(<Home></Home>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/about" element={ isSignedIn ?(<About></About>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects" element={ isSignedIn ?(<Projects></Projects>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/ScreenSize" element={ isSignedIn ?(<ScreenSize></ScreenSize>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/Counter" element={ isSignedIn ?(<Counter></Counter>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/ExcuseGenerator" element={ isSignedIn ?(<ExcuseGenerator></ExcuseGenerator>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/TodoList" element={ isSignedIn ?(<TodoList></TodoList>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/Calculator" element={ isSignedIn ?(<Calculator></Calculator>) :(<Navigate to="/signIn"></Navigate>)}></Route>
+        <Route path="/projects/QuizApp" element={ isSignedIn ?(<QuizApp></QuizApp>) :(<Navigate to="/signIn"></Navigate>)}></Route>
 
       </Routes>
     </div>
